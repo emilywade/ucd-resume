ef4d16b82bad1b5ea5dd47789face896947b7305
Emily Wade <emilywade314@gmail.com>
2025-02-10 19:28:54 +0000

    connecting to emailjs api

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,15 @@
+function sendEmail(contactForm) {
+    emailjs.send("service_n79jo81", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    )
+}
