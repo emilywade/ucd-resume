e3926868d1f6ad3a95adba5e79221b476c0cfd2b
Emily Wade <emilywade314@gmail.com>
2025-02-10 18:58:47 +0000

    amend error response message

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -66,6 +66,9 @@ function fetchGitHubInformation(event) {
         function (errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`)
+            } else if(errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+                $("#gh-user-data").html(`<h4>Too many requests! Please wait until ${resetTime.toLocaleDateTimeString()}</h4>`)
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
